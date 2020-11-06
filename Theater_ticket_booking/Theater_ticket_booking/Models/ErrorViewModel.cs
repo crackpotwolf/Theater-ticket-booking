c1d7e455791befd23dedcfb5ed014268098c0d10
crackpotwolf <artyushinvl@gmail.com>
2020-11-06 13:26:14 +0400

    Init commit

---
+++ Theater_ticket_booking/Theater_ticket_booking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Theater_ticket_booking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
